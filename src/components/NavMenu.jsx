df82d037757281a1e6e82c42228636402bd045d2
Miquéias Azevedo <43538720+miqueiasazevedo@users.noreply.github.com>
2025-04-24 14:31:42 +0000

    Implementa ThemeContext com suporte a tema claro e escuro

---
+++ src/components/NavMenu.jsx
@@ -0,0 +1,24 @@
+//Menu com 4 itens e um botão para alterar o tema
+import React from 'react';
+import { useContext } from 'react';
+import { ThemeContext } from '../theme/ThemeContext';
+
+function NavMenu() {
+    const { theme, toggleTheme } = useContext(ThemeContext);
+
+    return (
+        <nav>
+            <h4 className="logo">My Website</h4>
+            <ul id="nav-menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Services</a></li>
+            </ul>
+            <button onClick={toggleTheme}>
+                {theme === 'light' ? 'Switch to Dark Mode' : 'Switch to Light Mode'}
+            </button>
+        </nav>
+    );
+}
+export default NavMenu;
---
+++ src/theme/ThemeContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext();
+
+export function ThemeProvider({ children}) {
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        setTheme(prev => (prev === 'light' ? 'dark' : 'light'));
+    };
+
+    useEffect(() => {
+        document.body.className = '';
+        document.body.classList.add(theme);
+    }, [theme]);
+
+    return [
+        <ThemeContext.Provider value={{ theme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    ]
+}
